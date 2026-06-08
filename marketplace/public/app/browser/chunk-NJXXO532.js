f74edf8313ab57c22ad65587ae45f5ac54af0c01
Blackfist696 <douyez.pascal@gmail.com>
2026-06-09 00:04:26 +0200

    compilation du frontend et déplacement des fichiers compilés dans le dossier public/app/browser

---
+++ marketplace/public/app/browser/chunk-NJXXO532.js
@@ -0,0 +1,1 @@
+function s(r){if(!r)return"";let t=r.trim().replace(/^\/+/,"");return t.startsWith("assets/products/")?"/"+t:t.startsWith("products/")?"/assets/"+t:"/"+t}export{s as a};
--- marketplace/public/app/chunk-467MMBBF.js
+++
@@ -1,1 +0,0 @@
-var n={production:!0,apiUrl:"/project02"};export{n as a};
